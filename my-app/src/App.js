0f4f5941478df02e14833c504aec4672e9410e30
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-03-19 17:24:22 -0700

    vt - refactored audioplayer and added useEffect to set the volume

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,63 @@
-import React from "react";
+import React, { useState, useEffect, useRef, forwardRef } from "react";
 import GenreScatterChart from './GenreScatterChart';
 
 export default function App() {
+  const audioPlayer = useRef();
+  const [audioPlayerKey, setAudioPlayerKey] = useState(0);
+  const [audioPlayerUrl, setAudioPlayerUrl] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioPlayerGenre, setAudioPlayerGenre] = useState("");
+
+  function togglePlay(url, genre) {
+    if (audioPlayerUrl === `https://p.scdn.co/mp3-preview/${url}`) {
+      if (isPlaying) {
+        audioPlayer.current.pause();
+      } else {
+        audioPlayer.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    } else {
+      setAudioPlayerUrl(`https://p.scdn.co/mp3-preview/${url}`);
+      setAudioPlayerKey(audioPlayerKey + 1);
+      setIsPlaying(true);
+      setAudioPlayerGenre(genre);
+    }
+  };
+
   return (
     <div>
       <div className="brand">
-        <div className="app-title">EverySound</div>
+        <div className="app-title">MusicMap</div>
         <div>by Vincent Tieu</div>
       </div>
-      <GenreScatterChart />
+      <GenreScatterChart audioPlayerUrl={audioPlayerUrl} togglePlay={togglePlay} />
+      <div className="audio-player">
+        {audioPlayerGenre !== "" 
+          ? <AudioPlayer ref={audioPlayer} audioPlayerKey={audioPlayerKey} audioPlayerGenre={audioPlayerGenre} audioPlayerUrl={audioPlayerUrl} />
+          : <div>Drag to Navigate and Click to Listen!</div>
+        }
+      </div>
     </div>
   )
 }
+
+const AudioPlayer = forwardRef((props, ref) => {
+  const { audioPlayerKey, audioPlayerGenre, audioPlayerUrl } = props;
+  useEffect(() => {
+    if (ref) {
+      console.log(`genre: ${audioPlayerGenre}`);
+      ref.current.play();
+      ref.current.volume = 0.15;
+    }
+  }, [ref, audioPlayerGenre]);
+
+  return (
+    <div>
+      <div>Genre Playing: {audioPlayerGenre}</div>
+      <br/>
+      <audio ref={ref} key={audioPlayerKey} controls>
+        <source src={audioPlayerUrl} />
+      </audio>
+    </div>
+  )
+})
--- my-app/src/GenreScatterChart.js
+++ my-app/src/GenreScatterChart.js
@@ -1,20 +1,17 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { ScatterChart, Scatter, Cell, Tooltip, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { MapInteractionCSS } from 'react-map-interaction';
 import genre_data from "./everynoise-scrape.json"; // last added data-2
 // import genre_data from "./og-everynoise-scrape"; // complete
 // import genre_data from "./test"
 
-export default function GenreScatterChart() {
-  const audioPlayer = useRef();
-  const [audioPlayerKey, setAudioPlayerKey] = useState(0);
-  const [audioPlayerUrl, setAudioPlayerUrl] = useState("");
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [audioPlayerGenre, setAudioPlayerGenre] = useState("");
+export default function GenreScatterChart(props) {
+  const { audioPlayerUrl, togglePlay } = props;
   const [tooltipContent, setTooltipContent] = useState("");
 
   let keys = Object.keys(genre_data);
   let data = Object.values(genre_data);
+  let cells = [];
 
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
@@ -27,25 +24,6 @@ export default function GenreScatterChart() {
     return null;
   };
 
-  function togglePlay(url, genre) {
-    if (audioPlayerUrl === `https://p.scdn.co/mp3-preview/${url}`) {
-      console.log(isPlaying);
-      if (isPlaying) {
-        audioPlayer.current.pause();
-      } else {
-        audioPlayer.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    } else {
-      setAudioPlayerUrl(`https://p.scdn.co/mp3-preview/${url}`);
-      setAudioPlayerKey(audioPlayerKey + 1);
-      setIsPlaying(true);
-      setAudioPlayerGenre(genre);
-    }
-    // console.log(isPlaying);
-  };
-
-  let cells = [];
   for (let i = 0; i < data.length; i++) {
     cells.push(
       <Cell
@@ -60,34 +38,25 @@ export default function GenreScatterChart() {
   }
 
   return (
-    <>
-      <div className="audio-player">
-        <div>{audioPlayerGenre !== "" ? `Currently Playing: ${audioPlayerGenre}` : "Drag to Navigate and Click to Listen!"}</div>
-        <br/>
-        <audio ref={audioPlayer} key={audioPlayerKey} controls autoPlay>
-          <source src={audioPlayerUrl} />
-        </audio>
-      </div>
-      <MapInteractionCSS>
-        <ScatterChart
-          width={2400}
-          height={2400}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-        >
-          <CartesianGrid vertical={false} horizontal={false} />
-          <XAxis hide={true} type="number" dataKey="left" />
-          <YAxis hide={true} type="number" dataKey="top" />
-          <Tooltip content={<CustomTooltip />} />
-          <Scatter name="data" data={data} fill="#8884d8">
-            {cells}
-          </Scatter>
-        </ScatterChart>
-      </MapInteractionCSS>
-    </>
+    <MapInteractionCSS>
+      <ScatterChart
+        width={2400}
+        height={2400}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid vertical={false} horizontal={false} />
+        <XAxis hide={true} type="number" dataKey="left" />
+        <YAxis hide={true} type="number" dataKey="top" />
+        <Tooltip content={<CustomTooltip />} />
+        <Scatter name="data" data={data} fill="#8884d8">
+          {cells}
+        </Scatter>
+      </ScatterChart>
+    </MapInteractionCSS>
   );
 }
