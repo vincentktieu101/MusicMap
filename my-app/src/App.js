c019f0436cce2986ed7c565c52d1f2fb825545b3
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-03-19 21:18:17 -0700

    vt - added the complete db json file and a getNewMusicMap function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect, useRef, forwardRef } from "react";
-import GenreScatterChart from './GenreScatterChart';
+import MusicMap from "./MusicMap";
+import IconButton from "@material-ui/core/IconButton";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import initialData from "./scrapes/db.json";
 
 export default function App() {
   const audioPlayer = useRef();
   const [audioPlayerKey, setAudioPlayerKey] = useState(0);
   const [audioPlayerUrl, setAudioPlayerUrl] = useState("");
-  const [isPlaying, setIsPlaying] = useState(false);
   const [audioPlayerGenre, setAudioPlayerGenre] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    setData(getNewMusicMap(initialData));
+  }, []);
 
   function togglePlay(url, genre) {
     if (audioPlayerUrl === `https://p.scdn.co/mp3-preview/${url}`) {
@@ -22,23 +30,38 @@ export default function App() {
       setIsPlaying(true);
       setAudioPlayerGenre(genre);
     }
-  };
+  }
 
   return (
     <div>
       <div className="brand">
-        <div className="app-title">MusicMap</div>
+        <div className="app-title">
+          MusicMap
+          <IconButton onClick={() => setData(getNewMusicMap(initialData))}>
+            <RefreshIcon style={{ color: "white" }} />
+          </IconButton>
+        </div>
         <div>by Vincent Tieu</div>
       </div>
-      <GenreScatterChart audioPlayerUrl={audioPlayerUrl} togglePlay={togglePlay} />
+      <MusicMap
+        data={data}
+        audioPlayerUrl={audioPlayerUrl}
+        togglePlay={togglePlay}
+      />
       <div className="audio-player">
-        {audioPlayerGenre !== "" 
-          ? <AudioPlayer ref={audioPlayer} audioPlayerKey={audioPlayerKey} audioPlayerGenre={audioPlayerGenre} audioPlayerUrl={audioPlayerUrl} />
-          : <div>Drag to Navigate and Click to Listen!</div>
-        }
+        {audioPlayerGenre !== "" ? (
+          <AudioPlayer
+            ref={audioPlayer}
+            audioPlayerKey={audioPlayerKey}
+            audioPlayerGenre={audioPlayerGenre}
+            audioPlayerUrl={audioPlayerUrl}
+          />
+        ) : (
+          <div>Drag to Navigate and Click to Listen!</div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 const AudioPlayer = forwardRef((props, ref) => {
@@ -54,10 +77,25 @@ const AudioPlayer = forwardRef((props, ref) => {
   return (
     <div>
       <div>Genre Playing: {audioPlayerGenre}</div>
-      <br/>
+      <br />
       <audio ref={ref} key={audioPlayerKey} controls>
         <source src={audioPlayerUrl} />
       </audio>
     </div>
-  )
-})
+  );
+});
+
+function getNewMusicMap(arr) {
+  var result = new Array(n),
+    len = arr.length,
+    taken = new Array(len),
+    n = arr.length;
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    var x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
---
+++ my-app/src/MusicMap.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  ScatterChart,
+  Scatter,
+  Cell,
+  Tooltip,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { MapInteractionCSS } from "react-map-interaction";
+
+export default function MusicMap(props) {
+  let { data, audioPlayerUrl, togglePlay } = props;
+  const [tooltipContent, setTooltipContent] = useState("");
+
+  data = data.slice(0, 400);
+  let cells = [];
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return <div className="custom-tooltip">{tooltipContent}</div>;
+    }
+    return null;
+  };
+
+  for (let i = 0; i < data.length; i++) {
+    cells.push(
+      <Cell
+        onMouseOver={() => setTooltipContent(data[i].genre)}
+        onClick={() => togglePlay(data[i].preview_url, data[i].genre)}
+        key={`cell-${data[i].genre}`}
+        fill={data[i].color}
+        stroke={data[i].color}
+        strokeWidth={
+          audioPlayerUrl ===
+          `https://p.scdn.co/mp3-preview/${data[i].preview_url}`
+            ? 25
+            : 0
+        }
+      />
+    );
+  }
+
+  return (
+    <MapInteractionCSS>
+      <ScatterChart
+        width={2400}
+        height={2400}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid vertical={false} horizontal={false} />
+        <XAxis hide={true} type="number" dataKey="left" />
+        <YAxis hide={true} type="number" dataKey="top" />
+        <Tooltip content={<CustomTooltip />} />
+        <Scatter name="data" data={data} fill="#8884d8">
+          {cells}
+        </Scatter>
+      </ScatterChart>
+    </MapInteractionCSS>
+  );
+}
